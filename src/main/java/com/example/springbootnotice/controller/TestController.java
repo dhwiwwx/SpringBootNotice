8cb36bfb9be51d4bad176a2d2b3ca1cea7f648b8
dhwiwwx <dhwiwwx@naver.com>
2023-08-29 22:28:03 +0900

    RequestParam End

--- src/main/java/com/example/springbootnotice/controller/TestController.java
+++ src/main/java/com/example/springbootnotice/controller/TestController.java
@@ -1,15 +1,22 @@
 package com.example.springbootnotice.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
 
-    @GetMapping
-    public String testController(){
-        return "hello";
+    @GetMapping("/testGetMapping")
+    public String testControllerWithPath(){
+        return "Hello World! testGetMapping";
     }
+
+    @GetMapping("testRequestParam")
+    public String testControllerRequestParam(@RequestParam(required = false) int id){
+        return "Hello World! ID" + id;
+    }
+
+//    @GetMapping("/{id}")
+//    public String testControllerWithPathVariables(@PathVariable(required = false) int id){
+//        return "Hello World! ID" + id;
+//    }
 }
