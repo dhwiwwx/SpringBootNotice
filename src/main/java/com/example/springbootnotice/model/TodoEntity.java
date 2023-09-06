9e4f6287d846f55dc3548cea0e35d2b8daf72418
dhwiwwx <dhwiwwx@naver.com>
2023-09-06 17:53:19 +0900

    JPA 실습

--- src/main/java/com/example/springbootnotice/model/TodoEntity.java
+++ src/main/java/com/example/springbootnotice/model/TodoEntity.java
@@ -1,12 +1,14 @@
 package com.example.springbootnotice.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 @Builder
 @NoArgsConstructor
@@ -16,6 +18,8 @@ import lombok.NoArgsConstructor;
 @Table(name = "Todo")
 public class TodoEntity {
     @Id
+    @GeneratedValue(generator = "system-uuid") // id를 자동생성 하겠다.
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String id; // 이 오브젝트 아이디
     private String userId; // 이 오브젝트를 생성한 유저의 아이디
     private String title; // 타이틀 제목
--- src/main/java/com/example/springbootnotice/service/TodoService.java
+++ src/main/java/com/example/springbootnotice/service/TodoService.java
@@ -1,12 +1,24 @@
 package com.example.springbootnotice.service;
 
+import com.example.springbootnotice.model.TodoEntity;
+import com.example.springbootnotice.persistence.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 //@Service 기능적으로 비지니스 로직을 수행하는 서비스 레이어를 알려줌
 public class TodoService {
 
-    public String testService(){
-        return "Test Service";
-    }
+ @Autowired
+ private TodoRepository repository;
+
+ public String testService(){
+//     TodoEntity 생성
+     TodoEntity entity = TodoEntity.builder().title("My First todo item").build();
+//     TodoEntity 저장
+     repository.save(entity);
+//     TodoEntity 검색
+     TodoEntity saveEntity = repository.findById(entity.getId()).get();
+     return saveEntity.getTitle();
+ }
 }
